4673e2b4891d3cb645f9e1e14e25ab7fb939d01f
WoWjuuj <witherking@seznam.cz>
2024-08-08 19:13:43 +0200

    Funkční update

--- Discord-automessege.py
+++ Discord-automessege.py
@@ -4,11 +4,11 @@ from datetime import datetime
 import time
 
 # URLS of discord chats you want to send messeges
-target_set=["https://discord.com/api/v9/channels/1271041664412291102/messages"]
+target_set=[""]
 
 # Your Discord token -- Dont share this its Hash of your discord password :3
 headers = {
-    "Authorization": ""
+    ""
 }
 
 print("")
@@ -21,7 +21,7 @@ print("HHH    HHH   EEEEEEEE   LLLLLLL   LLLLLLL   OOOOOOO      FFF      RRR   R
 print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
 print("                                                                                                                     ")
 print("1. Number 1 = morning                                                                                                ")
-print("2. Number 2 = afternoon         5.If you want to start use -1                                                        ")
+print("2. Number 2 = afternoon         4.If you want to start use 4                                                         ")
 print("3. Number 3 = night                                                                                                  ")
 print("")
 
@@ -39,12 +39,19 @@ def get_info(day_part):
     return hour, minut, context
 
 while True:
-    DayTime = int((input("Part of day ? ")))
+    try:
+        DayTime = int((input("Part of day ? ")))
+    except ValueError:
+        print("Please enter a number", file=stderr)
+        continue
     options = {1:"Morning",2:"Afternoon",3:"Night"}
     if DayTime in options.keys():
         hour, minut, context = get_info(options[DayTime])
-    else:
+    elif DayTime == 4:
         break
+    else :
+        print("Please enter a number between 1 and 3", file=stderr)
+        continue
 
 
 def load_file(day_part):
@@ -55,8 +62,7 @@ def load_file(day_part):
             minut = int(lines[3].strip())
             context = lines[5].strip()
 
-        print("Morning")
-        print(f"Morning Hours: {hour} {minut}")
+        print(f"{day_part} Hours: {hour} {minut}")
         print(f"Your text: {context}\n")
         return hour, minut, context
     except FileNotFoundError:
@@ -64,32 +70,33 @@ def load_file(day_part):
         return None
 
 
-if DayTime == -1:
-    print(" ")
-    print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-    print("SSSSS  TTTTTT    A      RRRR    TTTTTT  ")
-    print("SS       TT     A A     R  RR     TT    ")
-    print("SSSS     TT    AAAAA    RRRR      TT    ")
-    print("   SS    TT   A     A   R  RR     TT    ")
-    print("SSSS     TT   A     A   R   RR    TT    ")
-    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+print(" ")
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("SSSSS  TTTTTT    A      RRRR    TTTTTT  ")
+print("SS       TT     A A     R  RR     TT    ")
+print("SSSS     TT    AAAAA    RRRR      TT    ")
+print("   SS    TT   A     A   R  RR     TT    ")
+print("SSSS     TT   A     A   R   RR    TT    ")
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
 
-    """remind_dict = {
-        "Morning":load_file("Morning"),
-        "Afternoon":load_file("Afternoon"),
-        "Night":load_file("Night")
-    }"""
+"""remind_dict = {
+    "Morning":load_file("Morning"),
+    "Afternoon":load_file("Afternoon"),
+    "Night":load_file("Night")
+}"""
 
-    """
-    remind_dict = {}
-    for key in {"Morning","Afternoon","Night"}:
-        remind_dict.update({key:load_file(key)})
-    """
-remind_dict = {
+remind_dict = {}
+for key in {"Morning", "Afternoon", "Night"}:
+    value = load_file(key)
+    if value is not None:
+        remind_dict.update({key: value})
+
+"""remind_dict = {
     key:load_file(key)
     for key in {"Morning","Afternoon","Night"}
     if load_file(key) is not None
-}
+}"""
+
 print(remind_dict)
 
 # if USA_OR_NORMAL == "am":
